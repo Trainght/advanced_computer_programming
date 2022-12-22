9aa175e198b7bd4f7ed208dbd224860a2d85a53d
Trainght/test.123 <nathachai.c@kkumail.com>
2022-12-22 23:03:09 +0700

    update lab2

--- charoenchai/nathachai/lab2/StringAPI.java
+++ charoenchai/nathachai/lab2/StringAPI.java
@@ -3,16 +3,21 @@ package charoenchai.nathachai.lab2;
 public class StringAPI {
     public static void main(String[] args) {
         int len_of_args = args.length; // lenght of input
-        String schoolName  = args[0]; // set args
+        String schoolInput  = args[0]; // set args
+        String schoolName_lower = schoolInput.toLowerCase();
         if (len_of_args == 1) {
-            if(schoolName.equalsIgnoreCase("university")){ // check string have "university" input args
-                System.out.println(schoolName+" is university");
+            int check_u = schoolName_lower.indexOf("university");
+            int check_c = schoolName_lower.indexOf("college");
+            boolean u = (check_u != -1);
+            boolean c = (check_c != -1);
+            if(u){ // check string have "university" input args
+                System.out.println(schoolInput+" is university");
             }
-            else if (schoolName.equalsIgnoreCase("college")){ // check string have "college" input args
-                System.out.println(schoolName+" is college");
+            else if (c){ // check string have "college" input args
+                System.out.println(schoolInput+" is college");
             }
             else{
-                System.out.print(schoolName + " is neither a university nor a college"); // if input argument is not have "university" and "college" 
+                System.out.print(schoolInput + " is neither a university nor a college"); // if input argument is not have "university" and "college" 
             }
         }
         else{
