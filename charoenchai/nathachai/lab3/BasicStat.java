5ea2faa4e82a9acc0e2468ad53c4287f652cf3ab
Trainght/test.123 <nathachai.c@kkumail.com>
2023-01-06 00:38:30 +0700

    add lab3

---
+++ charoenchai/nathachai/lab3/BasicStat.java
@@ -0,0 +1,74 @@
+/**
+* This BasicStat program to get numbers amount less than or equal 10 numbers to sort and find minimum, maximum, average, median
+* and standard deviation.
+*
+* Author: Nathachai Charoenchai
+* ID: 653040126-5
+* Sec: 1
+* Date: January 5, 2023
+*
+**/
+
+package charoenchai.nathachai.lab3;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BasicStat{
+    public static void main(String[] args) {
+        Scanner input_user = new Scanner(System.in);
+        System.out.print("Enter numbers (separated by space): ");
+        String input_num = input_user.nextLine(); // input with scanner (string)
+
+        if(input_num != ""){
+            String[] numsplit = input_num.split(" "); // list from input_num but split " "
+            double[] numDouble = new double[numsplit.length]; 
+            if(numsplit.length>10){
+                System.out.println("There are no more than 10 numbers to be enter"); // If you in put over than 10 number
+            }
+            else{
+                for (int i = 0; i < numDouble.length; i++) {
+                numDouble[i] = Double.parseDouble(numsplit[i]); // change string to double
+                }
+                System.out.print("Sorted numbers are ");
+                Arrays.sort(numDouble); // sort number in list numDouble
+                for (Double outnumsort : numDouble) {
+                    System.out.print(outnumsort + " ");    
+                }
+                //////////////// The minimum ////////////////
+                System.out.printf("\nMinimum: %.2f", numDouble[0]); //output Minimum
+                //////////////// The maximum ////////////////
+                int last = numDouble.length;
+                System.out.printf("\nMaximum: %.2f", numDouble[last-1]); // output Maximum
+                //////////////// The average ////////////////
+                double number_sum = 0;
+                for (int i = 0; i < numDouble.length; i++) {
+                    number_sum += numDouble[i];
+                }
+                double AvgNum = number_sum/numDouble.length ;
+                System.out.printf("\nAverage: %.2f",AvgNum); // output Average
+                //////////////// The median (odd and even) ////////////////
+                if (numsplit.length % 2 == 0) {
+                    double num_med = (numDouble[(numDouble.length / 2) - 1] + numDouble[numDouble.length / 2]) / 2;
+                    System.out.printf("\nMedian: %.2f", num_med); // output Median in even number
+                } else {
+                    System.out.printf("\nMedian: %.2f", AvgNum); // output Median in odd number
+                }
+                //////////////// The standard deviation ////////////////
+                double num_standard = 0;
+                for (int i = 0; i < numDouble.length; i++) {
+                    num_standard += Math.pow(numDouble[i] - AvgNum, 2); 
+                }
+                double standard = num_standard / numDouble.length;
+                System.out.printf("\nStandard deviation: %.2f", Math.sqrt(standard)); // output Standard deviation
+            }
+        }
+        else{
+            System.out.println("Please enter at least one number."); // If you not input something or not a number.
+        }
+    }       
+} 
+
+
+
+
--- charoenchai/nathachai/lab3/SicBoMethod.java
+++ charoenchai/nathachai/lab3/SicBoMethod.java
@@ -1,5 +1,5 @@
 /**
-* This SicBo program will be give you 2 options, option 1 is choose high or low numbers and option 2 is choose number between 1-6
+* This SicBoMethod program will be give you 2 options, option 1 is choose high or low numbers and option 2 is choose number between 1-6
 * if you choose some number and typing high and low or number 1-6 this program will random 3 dice and tell you win 20 bath or loose 10 bath.
 *
 * Author: Nathachai Charoenchai
@@ -10,24 +10,39 @@
 **/
 
 package charoenchai.nathachai.lab3;
+import java.util.Scanner;
 
-import java.util.Scanner; // Import scanner to use in program
-import java.lang.Math; // Import math to use in program
+public class SicBoMethod {   
+    static int choice;
+    static Scanner input = new Scanner(System.in); // scanner
+    public static void main(String[] args) { // Main method
+        getChoice(); 
+        playGame(); 
+    }
 
-public class SicBo {
-    public static void main(String[] args) {
-        System.out.println("Choose how you want to bet:\nType 1 for choosing high or low numbers.\nType 2 for choosing number between 1-6."); // Output text to tell user how to input
-        System.out.print("Enter yourchoice: "); // Output text to tell user
-        Scanner choice = new Scanner(System.in); // Create a Scanner object
-        int check_choice; // Set input to integer
-        check_choice = choice.nextInt(); // Read user input
+    static void getChoice(){ // getChoice method
+        System.out.println("Choose how you want to bet:\nType 1 for choosing high or low numbers.\nType 2 for choosing number between 1-6.");
+        System.out.print("Enter yourchoice: ");
+        int check_choice;
+        check_choice = input.nextInt();
+        if (check_choice == 1 || check_choice == 2) { // if input is 1 or 2
+            choice = check_choice;
+        }
+        else {
+            System.out.print("Incorrect input. Enter 1 or 2 only.");
+        }
+    }
+
+    static void playGame(){ // playGame method
         Integer dice1 = 1+(int)(Math.random()*((6-1)+1)); // Random dice1
         Integer dice2 = 1+(int)(Math.random()*((6-1)+1)); // Random dice2
         Integer dice3 = 1+(int)(Math.random()*((6-1)+1)); // Random dice3
         Integer total = dice1+dice2+dice3; // Total dice of 3
+        int check_choice;
+        check_choice = choice;
         if (check_choice == 1){ // User choose Type 1
             System.out.print("Type in h for high and l for low: "); // Output text to tell user to choose "h" high or "l" low 
-            String hl_input = choice.next(); // Read user input "l" "h" or other
+            String hl_input = input.next(); // Read user input "l" "h" or other
             if(hl_input.equalsIgnoreCase("h")){ // If input is h and run below the program 
                 System.out.println("Dice 1 : "+ dice1 +", Dice 2 : "+ dice2 +", Dice 3 : "+ dice3 ); // Output result 3 dice
                 System.out.println("Total is "+total); // Output total dice of 3
@@ -36,9 +51,9 @@ public class SicBo {
                 }
                 else{ // More than 9 is equal high you win
                     System.out.println("You win 20 Bath");
-                }   
+                }
             }
-            else if(hl_input.equalsIgnoreCase("l")){ // If input is l and run below the program 
+            else if(hl_input.equalsIgnoreCase("l")){
                 System.out.println("Dice 1 : "+ dice1 +", Dice 2 : "+ dice2 +", Dice 3 : "+ dice3 ); // Output result 3 dice
                 System.out.println("Total is "+total); // Output total dice of 3
                 if(total>0 && total <=9){ // If the total is over 0 but less than or equal to 9 you win 20 Bath
@@ -52,10 +67,10 @@ public class SicBo {
                 System.out.println("Incorrect input. Enter h for high and l for low only.");
             }
         }
-        else if(check_choice == 2){ //If user choose Type 2
+        else if(check_choice == 2){
             System.out.print("Type in a number to bet on (1-6): "); // Output text to tell user to choose number between 1 and 6
-            String num_input = choice.next(); // Read user input
-            Integer num_check = Integer.parseInt(num_input); // Change string to integer 
+            String num_input = input.next(); // Read user input
+            Integer num_check = Integer.parseInt(num_input); // Change string to integer
             int[] dice = new int[3]; // Create list dice
             dice[0] = dice1; // Set position 0 equal dice 1
             dice[1] = dice2; // Set position 1 equal dice 2
