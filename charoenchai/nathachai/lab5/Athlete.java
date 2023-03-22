4dbdfdc3ef7cb2cb393beaa1152ad5104c510e22
Trainght/test.123 <nathachai.c@kkumail.com>
2023-03-22 21:54:04 +0700

    update Athlete.java

--- charoenchai/nathachai/lab5/Athlete.java
+++ charoenchai/nathachai/lab5/Athlete.java
@@ -11,59 +11,76 @@ package charoenchai.nathachai.lab5;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+// import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
+import java.io.*;
 
 //Class has protected instance variables to record the following information.
-public class Athlete {
+public class Athlete implements Serializable {
 
-    protected String name ;
-    protected double weight ;
-    protected double height ;
-    protected Gender gender ;
-    protected String nationality ;
-    protected LocalDate birthdate ;
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    protected String name;
+    protected double weight;
+    protected double height;
+    protected Gender gender;
+    protected String nationality;
+    protected String birthdate;
+
+    protected LocalDate dob;
+
+    // DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
     // Method information: name, weight, height, gender, nationality, birthdate.
     public Athlete(String name, double weight, double height, Gender gender, String nationality, String birthdate) {
-    this.name = name ;
-    this.weight = weight ;
-    this.height = height ;
-    this.gender = gender ;
-    this.nationality = nationality ;
-    this.birthdate = LocalDate.parse(birthdate, formatter);
+        this.name = name;
+        this.weight = weight;
+        this.height = height;
+        this.gender = gender;
+        this.nationality = nationality;
+        this.birthdate = birthdate;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        this.dob = LocalDate.parse(birthdate, formatter);
+
+        // this.birthdate = LocalDate.parse(birthdate, formatter);
     }
+
     // this method is will set new height from user input.
-    public Double setHeight(double inHeight){
-        return height =+ inHeight ;
+    public Double setHeight(double inHeight) {
+        return height = +inHeight;
     }
+
     // Method is will return name in this class.
     public String getName() {
         return this.name;
     }
-    // Method is will return weight in this class.   
-    public Double getWeight(){
-        return this.weight ;
+
+    // Method is will return weight in this class.
+    public Double getWeight() {
+        return this.weight;
     }
+
     // Method is will return height in this class.
-    public Double getHeight(){
-        return this.height ;
+    public Double getHeight() {
+        return this.height;
     }
+
     // Method is will return gender in this class.
-    public Gender getGnder(){
-        return this.gender ;
+    public Gender getGnder() {
+        return this.gender;
     }
+
     // Method is will return nationality in this class.
-    public String getNationality(){
-        return this.nationality ;
+    public String getNationality() {
+        return this.nationality;
     }
+
     // Method is will return birthdate in this class.
-    public LocalDate getBirthdate (){
-        return this.birthdate ;
+    public LocalDate getBirthdate() {
+        return this.dob;
     }
+
     // This method is will compare age between two athlet.
     public void compareAge(Athlete B) {
-        LocalDate dateBefore = birthdate;
+        LocalDate dateBefore = dob;
         LocalDate dateAfter = B.getBirthdate();
         int year = (int) ChronoUnit.YEARS.between(dateBefore, dateAfter);
         if (year > 0) {
@@ -74,14 +91,17 @@ public class Athlete {
             System.out.println(name + " is as old as " + B.getName());
         }
     }
-//this is override using twostring method for this play the name weight height gender nationnality and birthdate. 
-@Override
-public String toString() {
-    return "Athlete ["+ this.name + ","+this.weight+"kg, "+this.height+"m, "+this.gender + ", "+this.nationality+ ", " +this.birthdate+" ]";
-}
-//class enum gender
-public enum Gender {
-    MALE, FEMALE
-}
-}
 
+    // this is override using twostring method for this play the name weight height
+    // gender nationnality and birthdate.
+    @Override
+    public String toString() {
+        return "Athlete [" + this.name + ", " + this.weight + "kg, " + this.height + "m, " + this.gender + ", "
+                + this.nationality + ", " + this.birthdate + " ]";
+    }
+
+    // class enum gender
+    public enum Gender {
+        MALE, FEMALE
+    }
+}
